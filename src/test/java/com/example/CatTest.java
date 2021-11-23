2751f4509e2b817da8c7e1be1392b62108795d01
AlexanderSharygin <52005798+AlexanderSharygin@users.noreply.github.com>
2021-11-23 19:07:46 +0300

    CatCovered
    The Cat class is covered by testsa

---
+++ src/test/java/com/example/CatTest.java
@@ -0,0 +1,39 @@
+package com.example;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+
+
+@RunWith(MockitoJUnitRunner.class)
+public class CatTest {
+    @Mock
+   Feline feline;
+
+@Mock
+Animal animal;
+
+
+    @Test
+    public void testGetSound() {
+        Cat cat = new Cat(feline);
+        String expected = "Мяу";
+        String actual = cat.getSound();
+        Assert.assertEquals("Incorrect getSound method result",expected,actual);
+
+
+    }
+
+    @Test
+    public void testGetMeat() throws Exception {
+
+        Cat cat = new Cat(feline);
+        cat.getFood();
+        Mockito.verify(feline).eatMeat();
+
+    }
+
+}
